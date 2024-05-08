cd7ea6d3daf20c9325d80d45c7811783a1fecf81
Marcelo Machado Fleury <marcelomf@gmail.com>
2024-05-08 18:07:17 -0300

    fix

--- lib/esm/puppeteer/generated/version.d.ts
+++ lib/esm/puppeteer/generated/version.d.ts
@@ -1,5 +1,5 @@
 /**
  * @internal
  */
-export declare const packageVersion = "22.8.7";
+export declare const packageVersion = "22.8.8";
 //# sourceMappingURL=version.d.ts.map
--- src/generated/version.ts
+++ src/generated/version.ts
@@ -1,4 +1,4 @@
 /**
  * @internal
  */
-export const packageVersion = '22.8.7';
+export const packageVersion = '22.8.8';
